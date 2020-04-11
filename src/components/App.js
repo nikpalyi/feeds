18ab47c83ad4e81a8a4ac0082626f92caa8ca83a
nikalyi <nikalyi@gmail.com>
2020-04-11 12:02:49 +0100

    using template literals in url to reuse

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import News from './News/News.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My feed</h1>
-      </header>
-      <News />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      news1: { 
+      type: 'top-headlines',
+      query: 'sources=bbc-news'
+      },
+      news2: { 
+        type: 'everything',
+        query: 'domains=techcrunch.com&language=en'
+        }     
+    };
+  }
 
-export default App;
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <header className='App-title'><h1>My feed</h1></header> 
+        </header>
+        <News news={this.state.news1}/>
+        <News news={this.state.news2} />
+      </div>
+    );
+  
+  }
+} 
+
+export default App; 
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -6,12 +6,13 @@ class News extends Component {
         super(props);
         this.state= {
             news: []
-        }
-
+         
+        
+        };
     }
 
     componentDidMount() {
-        const url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=88b6436c3a974d07a400c9a2900adc02';
+        const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=88b6436c3a974d07a400c9a2900adc02`;
 
         fetch(url)
             .then((response) => {
@@ -22,7 +23,7 @@ class News extends Component {
                     news: data.articles
                 })
             })
-            .catch((error) => console.log('ERROR',error));
+            .catch((error) => console.log(error));
         }
     
 
