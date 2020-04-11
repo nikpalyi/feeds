0937a90b7d316dc07d208249b4e3d14c24265616
nikalyi <nikalyi@gmail.com>
2020-04-11 14:36:27 +0100

    adding methods for axios

--- src/components/News/SideNews.js
+++ src/components/News/SideNews.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SingleSide from './SingleSide';
+import axios from 'axios'
 
 
 class SideNews extends Component {
@@ -11,12 +12,20 @@ class SideNews extends Component {
     }
 
     componentDidMount() {
-    
+        const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=88b6436c3a974d07a400c9a2900adc02`;
+
+        axios.get(url)
+            .then((response) => {
+                this.setState({
+                    sidenews: response.data.articles
+                })
+            })
+            .catch((error) => console.log(error))
     }
     
 
     renderItems() {
-        return this.state.news.map((item) => (
+        return this.state.sidenews.map((item) => (
             <SingleSide key={item.url} item={item} />
         ));
     }
